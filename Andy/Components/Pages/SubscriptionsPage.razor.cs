508a7af53230edf99a608edc3a639db14e46312e
Curby <curtisschmede670@gmail.com>
2025-10-25 01:12:45 +0200

    a bunch of UI

---
+++ Andy/Components/Pages/SubscriptionsPage.razor.cs
@@ -0,0 +1,22 @@
+﻿using Andy.Core.DTOs;
+using Andy.Core.Interfaces;
+using Microsoft.AspNetCore.Components;
+
+namespace Andy.Components.Pages
+{
+    public partial class Subscriptions
+    {
+        [Inject]
+        protected ISubscitionService SubscriptionService { get; set; } = default!;
+
+        [Inject]
+        protected ILogger<Subscriptions> Logger { get; set; } = default!;
+        protected IEnumerable<SubscriptionDto>? subscriptions;
+
+        protected override async Task OnInitializedAsync()
+        {
+            Logger.LogInformation("Subscriptions page initializing.");
+            subscriptions = await SubscriptionService.GetAllSubscriptionsAsync();
+        }
+    }
+}
