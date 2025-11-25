8cd4eb5dbbbee30808b343c9a71c9a49e138af9b
Curby <curtisschmede670@gmail.com>
2025-11-25 23:52:32 +0100

    SuppressMessage-Attribut für CA1873 hinzugefügt
    Eine neue Datei `GlobalSuppressions.cs` wurde hinzugefügt, um SuppressMessage-Attribute auf Projektebene zu verwalten.
    
    Ein SuppressMessage-Attribut wurde auf Assembly-Ebene hinzugefügt, um die Code-Analyse-Warnung "CA1873: Potenziell kostspielige Protokollierung vermeiden" für die Methode `AktivateAsync` in der Klasse `SubscriptionsPage` zu unterdrücken. Die Begründung für die Unterdrückung ist aktuell mit "<Ausstehend>" angegeben.

---
+++ Andy/GlobalSuppressions.cs
@@ -0,0 +1,8 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Performance", "CA1873:Potenziell kostspielige Protokollierung vermeiden", Justification = "<Ausstehend>", Scope = "member", Target = "~M:Andy.Components.Pages.SubscriptionsPage.AktivateAsync(Andy.ViewModels.SubscriptionViewModel)~System.Threading.Tasks.Task")]
